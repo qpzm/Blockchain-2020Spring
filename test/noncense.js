fc28aebe04e974d0f1de728cb140c6d2060f3e63
sms7002 <sms7002@gmail.com>
2020-06-09 17:38:08 +0900

    Add tests of updatePost

--- test/noncense.js
+++ test/noncense.js
@@ -35,4 +35,27 @@ contract("Noncense", accounts => {
     // e.g. expect([1, 2]).not.to.equal([1,2]) because [1,2] !== [1,2]
     expect(childIds.map(x => x.toNumber())).to.eql([childId]);
   });
+
+  it("updatePost() should be executed if the post is not immutable", async () => {
+    await noncense.newPost("Not immutable post", 0, "", "", false);
+    const postId = 1
+
+    await noncense.updatePost(postId, "Updated post", "", "");
+
+    const updatedPost = await noncense.post.call(postId);
+    expect(updatedPost.body).to.equal("Updated post");
+  });
+
+  it("updatePost() should NOT be executed if the post is immutable", async () => {
+    await noncense.newPost("Immutable post", 0, "", "", true);
+    const postId = 1
+
+    try {
+      await noncense.updatePost(postId, "Updated post", "", "");
+      assert.fail("The transaction should have thrown an error");
+    }
+    catch (err) {
+        assert.include(err.message, "revert", "The error message should contain 'revert'");
+    }
+  });
 });
